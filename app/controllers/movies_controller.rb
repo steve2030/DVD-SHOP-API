912c414ef061fb67642ae60df70c773dd0e75b3d
steve <stevemighty2030@gmail.com>
2022-11-27 11:01:41 +0300

    the lab is complete

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,10 @@
+class MoviesController < ApplicationController
+  def index
+    movies =Movie.all
+    render json:movies
+  end
+  def show
+    movie=Movie.find(params[:id])
+    render json:movie
+  end
+end
